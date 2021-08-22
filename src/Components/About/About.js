0db576806bdf1167ff8aa37ab0f0a4142099169e
Ritik Nandwal <ritiknandwal021@gmail.com>
2021-08-23 00:45:02 +0530

    Update about section and remove typed text

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -11,7 +11,14 @@ const About = () => {
                 <img src={ProfilePic} className="picContainer" />
                 <div className="abtContent">
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        I am a final year undergraduate at SGSITS, Indore ,pursuing
+                        my B.Tech in Computer Science and Engineering.I am a Computer Vision
+                        Enthusiast , and I really love that how we could use deep learning to
+                        improve human life and existing applications.I have also worked on Full-Stack
+                        web and mobile applications and I am already loving it.I am passionate about learning
+                        new things and when I find something really interesting I love to dig deeper into the
+                        nitty gritties of how they work.<br/>
+                        In my free time I play Guitar or I would simply go out for Cycling.
                     </p>
                 </div>
             </div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import socialLinks from '../socialLinks'
-import Typed from 'react-typed'
-import Particles from 'react-particles-js'
-import Rotater from '../Rotater'
-import Navbar from '../Navbar/Navbar'
 import './Home.css'
 const Home = () => {
     return (
         <>
-            
             <div className="container" id="Home">
                 <div className="main-info">
-                    <h1>Hi,I am Ritik Nandwal</h1>
+                    <h1>HI,I AM RITIK NANDWAL</h1>
                     <h3>I am final year undergraduate pursuing B.Tech in Computer Science Engineering <br />
                         from SGSITS Indore</h3>
-                    <Typed
-                        className="typed-text"
-                        strings={["Computer Vision Enthusiast", "Full Stack Web and Mobile Application Developer"]}
-                        typeSpeed={40}
-                        backSpeed={10}
-                        loop
-                    />
+                    <div className="resBtn" >
+                        <a
+                            href="https://rb.gy/kufujq"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{textDecoration:"none"}}
+                        >
+                            RESUME
+                        </a>
+                    </div>
                     <div className="table">
                         <ul className="social">
                             {socialLinks.map(
